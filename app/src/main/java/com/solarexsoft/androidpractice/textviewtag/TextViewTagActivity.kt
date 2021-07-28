b6d09dda0a23f0aca1ad1fc4d0504639190b878c
Solarex <rh.hou.work@gmail.com>
2021-07-28 16:26:40 +0800

    textviewtag

--- app/src/main/java/com/solarexsoft/androidpractice/textviewtag/TextViewTagActivity.kt
+++ app/src/main/java/com/solarexsoft/androidpractice/textviewtag/TextViewTagActivity.kt
@@ -15,9 +15,9 @@ class TextViewTagActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_textviewtag)
-        val spannableString = SpannableString("       哈利·波特邀请Cho参加舞会")
-        val span = TextViewTagSpan(this, R.drawable.ic_launcher_foreground, 2)
-        spannableString.setSpan(span,0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        val spannableString = SpannableString("      哈利·波特邀请Cho参加舞会哈利·波特邀请Cho参加舞会哈利·波特邀请Cho参加舞会哈利·波特邀请Cho参加舞会哈利·波特邀请Cho参加舞会哈利·波特邀请Cho参加舞会")
+        val span = TextViewTagSpan(this, R.drawable.vd_tag, 2)
+        spannableString.setSpan(span,0, 5, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         tv.text = spannableString
     }
 }
