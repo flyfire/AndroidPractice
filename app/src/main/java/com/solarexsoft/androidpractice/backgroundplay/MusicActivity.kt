a1df8fedd8234f2cb21fa87ce180f19bfb43fac6
Solarex <rh.hou.work@gmail.com>
2021-07-29 16:30:11 +0800

    implementations

---
+++ app/src/main/java/com/solarexsoft/androidpractice/backgroundplay/MusicActivity.kt
@@ -0,0 +1,10 @@
+package com.solarexsoft.androidpractice.backgroundplay
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by houruhou on 2021/7/29/4:13 PM
+ * Desc:
+ */
+class MusicActivity: AppCompatActivity() {
+}
---
+++ app/src/main/java/com/solarexsoft/androidpractice/backgroundplay/MusicService.kt
@@ -0,0 +1,8 @@
+package com.solarexsoft.androidpractice.backgroundplay
+
+/**
+ * Created by houruhou on 2021/7/29/4:14 PM
+ * Desc:
+ */
+class MusicService {
+}
