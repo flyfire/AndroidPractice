5816dbc9df3538fc0a0d1709cf6ef4aa8bb5f456
Solarex <rh.hou.work@gmail.com>
2021-10-13 19:40:18 +0800

    test

--- app/src/main/java/com/solarexsoft/androidpractice/MainActivity.kt
+++ app/src/main/java/com/solarexsoft/androidpractice/MainActivity.kt
@@ -2,9 +2,13 @@ package com.solarexsoft.androidpractice
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.solarexsoft.androidpractice.nestedscrolling.NestedScrollingActivityL
 import com.solarexsoft.androidpractice.textviewtag.TextViewTagActivity
+import com.tencent.mmkv.MMKV
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +21,14 @@ class MainActivity : AppCompatActivity() {
         textViewTag.setOnClickListener {
             startActivity(Intent(this, TextViewTagActivity::class.java))
         }
+        MMKV.initialize(this)
+        val mmkv = MMKV.mmkvWithID("solarex")
+        val test = mmkv.decodeParcelable("123", Test::class.java)
+        Log.d("solarex", "test = $test")
     }
 }
+
+@Parcelize
+data class Test(
+    val name: String
+) : Parcelable
--- app/src/main/java/com/solarexsoft/androidpractice/backgroundplay/MusicService.kt
+++ app/src/main/java/com/solarexsoft/androidpractice/backgroundplay/MusicService.kt
@@ -1,8 +1,26 @@
 package com.solarexsoft.androidpractice.backgroundplay
 
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import androidx.media.MediaBrowserServiceCompat
+
 /**
  * Created by houruhou on 2021/7/29/4:14 PM
  * Desc:
  */
-class MusicService {
+class MusicService : MediaBrowserServiceCompat(){
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
 }
